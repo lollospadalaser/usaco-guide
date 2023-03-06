b19fe22fb2157d7cf37e4513462eaa4880212d7f
Nathan Wang <nathan.r.wang@gmail.com>
2023-03-05 22:53:22 -0500

    (wip) add cool background effects to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,19 +58,37 @@ export default function IndexPage(): JSX.Element {
     <Layout>
       <SEO title={null} />
 
-      {/*<a*/}
-      {/*  href="http://usaco.org/"*/}
-      {/*  className="block text-base font-medium text-center text-white bg-blue-600 py-2"*/}
-      {/*>*/}
-      {/*  &larr; Back to usaco.org*/}
-      {/*</a>*/}
-      <div className="bg-black">
-        <TopNavigationBar />
-      </div>
-
       {/* Begin Hero */}
-      <div className="-mt-16 bg-gray-100 dark:bg-black">
-        <div className="md:min-h-screen flex flex-col px-4 sm:px-6 lg:px-8">
+      <div className="isolate bg-gray-100 dark:bg-black flex flex-col md:min-h-screen">
+        <TopNavigationBar />
+
+        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+          <svg
+            className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+            viewBox="0 0 1155 678"
+          >
+            <path
+              fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+              fillOpacity=".3"
+              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+            />
+            <defs>
+              <linearGradient
+                id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                x1="1155.49"
+                x2="-78.208"
+                y1=".177"
+                y2="474.645"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#9089FC" />
+                <stop offset={1} stopColor="#FF80B5" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+
+        <div className="flex-1 flex flex-col px-4 sm:px-6 lg:px-8 relative overflow-hidden">
           <div className="h-6 sm:h-12"></div>
 
           <div className="flex-1 flex flex-col justify-center">
@@ -184,6 +202,32 @@ export default function IndexPage(): JSX.Element {
             </a>
           </div>
           <div className="h-4 sm:h-6 md:h-16"></div>
+
+          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+            <svg
+              className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+            >
+              <path
+                fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
         </div>
       </div>
       {/* End Hero */}
